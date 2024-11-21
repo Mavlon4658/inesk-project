21ddef331db502aaa8f065d38e7811ca435c2113
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-11-21 15:07:31 +0500

    modal the end

--- js/main.js
+++ js/main.js
@@ -17,3 +17,30 @@ document.querySelectorAll('.direction .swiper-slide .main-img').forEach((el, elI
         directionSwp.slideTo(elID);
     }
 })
+
+
+let modal_one_btn = document.querySelectorAll('.modal_one_btn');
+let modal_two_btn = document.querySelectorAll('.modal_two_btn');
+let modal_one = document.querySelector('.modal_one');
+let modal_two = document.querySelector('.modal_two');
+let modal_bg = document.querySelector('.modal_bg');
+
+modal_one_btn.forEach(btn => {
+    btn.addEventListener('click', () => {
+        modal_one.classList.add('active');
+        modal_bg.classList.add('active');
+    });
+});
+
+modal_two_btn.forEach(btn => {
+    btn.addEventListener('click', () => {
+        modal_two.classList.add('active');
+        modal_bg.classList.add('active');
+    });
+});
+
+modal_bg.addEventListener('click', () => {
+    modal_one.classList.remove('active');
+    modal_two.classList.remove('active');
+    modal_bg.classList.remove('active');
+})
